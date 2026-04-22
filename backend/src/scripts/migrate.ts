971714d118ab418f11775082597b42f0c64890d2
Ramon <r.hentges@hotmail.com>
2026-04-22 18:57:51 -0300

    refactor: move migration script to scripts module
    Issue #local

---
+++ backend/src/scripts/migrate.ts
@@ -0,0 +1,19 @@
+import { identityDbConfig } from "../config/databases/identity_context";
+import { spawn } from "child_process";
+import path from "path";
+
+Object.assign(process.env, {
+  PGHOST: identityDbConfig.host,
+  PGPORT: String(identityDbConfig.port),
+  PGDATABASE: identityDbConfig.database,
+  PGUSER: identityDbConfig.user,
+  PGPASSWORD: identityDbConfig.password,
+});
+
+const action = process.argv[2] || "up";
+const migrationsDir = path.resolve(
+  __dirname,
+  "../migrations/identity_context/*.sql",
+);
+const args = ["-m", migrationsDir, "--use-glob", action];
+spawn("node-pg-migrate", args, { stdio: "inherit", env: process.env });
